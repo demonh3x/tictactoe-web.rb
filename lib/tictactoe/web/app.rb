ca9dae198a88d5b8992f5733a4b12f795e8f1c6e
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-28 16:41:23 +0100

    Removes example and adds three ignored integration tests

--- lib/tictactoe/web/app.rb
+++ lib/tictactoe/web/app.rb
@@ -2,7 +2,6 @@ module Tictactoe
   module Web
     class App
       def call(environment)
-        [200, {}, ['{"a": [1, 2, 3]}']]
       end
     end
   end
--- spec/tictactoe/web/app_spec.rb
+++ spec/tictactoe/web/app_spec.rb
@@ -8,11 +8,48 @@ RSpec.describe Tictactoe::Web::App do
   let(:env)      { Server::Environment.new }
   let(:response) { browser.get_response(env.hash) }
   
-  it 'creates a new game' do
-    env.set_uri('/new')
+  xit 'can start a game' do
+    env.set_uri('/start')
     expect(response.code).to eq 200
     expect(response.data).to eq({
-      "a" => [1, 2, 3]
+      "marks" => [
+        nil, nil, nil,
+        nil, nil, nil,
+        nil, nil, nil,
+      ],
+      "is_finished" => false,
+      "winner" => nil,
+      "available_moves" => [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    })
+  end
+
+  xit 'can go to the next step' do
+    env.set_uri('/start/0')
+    expect(response.code).to eq 200
+    expect(response.data).to eq({
+      "marks" => [
+        "x", nil, nil,
+        nil, nil, nil,
+        nil, nil, nil,
+      ],
+      "is_finished" => false,
+      "winner" => nil,
+      "available_moves" => [1, 2, 3, 4, 5, 6, 7, 8]
+    })
+  end
+
+  xit 'can get to a finished game' do
+    env.set_uri('/start/0/1/3/4/6')
+    expect(response.code).to eq 200
+    expect(response.data).to eq({
+      "marks" => [
+        "x", "o", nil,
+        "x", "o", nil,
+        "x", nil, nil,
+      ],
+      "is_finished" => true,
+      "winner" => "x",
+      "available_moves" => []
     })
   end
 end
